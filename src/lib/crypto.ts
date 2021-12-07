3453d32ed6e1c2ce3394eb5637ca72cdb8674c58
BAUDART Simon <bdrsmn@forem.be>
2021-12-07 16:04:11 +0100

    Use SHA512 and not Sha256

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -7,7 +7,7 @@ export const encrypt = (text: string, passPhrase: string, iv: Buffer): Buffer =>
 
     const localSecret = getServerConfiguration().secretKey + passPhrase;
 
-    const key = crypto.pbkdf2Sync(localSecret, passPhrase, 42000, 32, 'sha256');
+    const key = crypto.pbkdf2Sync(localSecret, passPhrase, 42000, 32, 'sha512');
 
     const cipher = crypto.createCipheriv(algorithm, key, iv);
 
@@ -18,7 +18,7 @@ export const encrypt = (text: string, passPhrase: string, iv: Buffer): Buffer =>
 
 export const decrypt = (encrypted: string, iv: string, passPhrase: string): string => {
     const localSecret = getServerConfiguration().secretKey + passPhrase;
-    const key = crypto.pbkdf2Sync(localSecret, passPhrase, 42000, 32, 'sha256');
+    const key = crypto.pbkdf2Sync(localSecret, passPhrase, 42000, 32, 'sha512');
 
     const decipher = crypto.createDecipheriv(algorithm, key, Buffer.from(iv, 'hex'));
     const decrypted = Buffer.concat([decipher.update(Buffer.from(encrypted, 'hex')), decipher.final()]);
