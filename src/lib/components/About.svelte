36da43f132a6200f3dbb23261469c3f1e65416b3
BAUDART Simon <bdrsmn@forem.be>
2021-12-07 15:58:36 +0100

    Add comment in about

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -15,8 +15,12 @@
 			The only limitation is a rate limit to avoid spam.<br />
 			I hope you will use it with respect of the work being done.
 
-			<h2 class="pt-3 pb-2">What is the <u>Shortener</u> ?</h2>
-			It's a very simple tool that converts the long URL into a short one, with an id instead of the loooooooooong path, it looks better in documents and presentations.
+			<h2 class="pt-3 pb-2">What is the <u>Secret Share</u> ?</h2>
+			It's a simple tool that encrypt a secret with a pass phrase and a secret key.
+			<ul>
+				<li>You can only decrypt the secret here, because the secret key is on the server.</li>
+				<li>We cannot decrypt the secret without you, because we don't know your passphrase.</li>
+			</ul>
 
 			<h2 class="pt-3 pb-2">What is tracked ?</h2>
 			Short answer is nothing. The long answer is, of course there are some technical logs on the servers, but without any user informations.<br />
@@ -29,17 +33,17 @@
 			Of course, these tools are not free to run. I make these because I really like the
 
 			<p>If you want to support this project, you are welcome to :</p>
-            <ul>
-                <li>
-                    <a href="https://buy.stripe.com/fZe29I2zJ1co9peaEE">Buy me a coffee (2.5€)</a>
-                </li>
-                <li>
-                    <a href="https://buy.stripe.com/fZebKi7U38EQ9peeUV">Buy me a beer (5.0€)</a>
-                </li>
-                <li>
-                    <a href="https://buy.stripe.com/14k7u23DNcV69pe28a">Buy me a rhum (10.0€)</a>
-                </li>
-            </ul>
+			<ul>
+				<li>
+					<a href="https://buy.stripe.com/fZe29I2zJ1co9peaEE">Buy me a coffee (2.5€)</a>
+				</li>
+				<li>
+					<a href="https://buy.stripe.com/fZebKi7U38EQ9peeUV">Buy me a beer (5.0€)</a>
+				</li>
+				<li>
+					<a href="https://buy.stripe.com/14k7u23DNcV69pe28a">Buy me a rhum (10.0€)</a>
+				</li>
+			</ul>
 			<p />
 
 			<h2 class="pt-3 pb-2">Another way to support ?</h2>
